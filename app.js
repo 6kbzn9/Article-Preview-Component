6ca2d5791873c02e41928e50bc5812bc0fb7d933
khaled <shnbyakuya@gmail.com>
2021-08-22 13:49:09 +0100

    mobile version finished ~

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const share = document.querySelector(".share");
 const links = document.querySelectorAll(".links");
 
-share.addEventListener("click", () => {
+share.addEventListener("click", (e) => {
+  e.preventDefault();
   links.forEach((link) => {
     link.classList.toggle("active");
   });
